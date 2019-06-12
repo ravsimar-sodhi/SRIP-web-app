b1466da55756aa91582f5030c2388dc072b97698
Ravsimar Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2019-06-12 18:02:47 +0530

    overhaul due to custom user

--- registration/admin.py
+++ registration/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, User, Mentor
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(User, UserAdmin)
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['handle', 'name', 'status', 'mentor','function_points', 'effort', 'report']
 
 admin.site.register(Student, StudentAdmin)
 
+class MentorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Mentor, MentorAdmin)
 # class UserStudentAdmin(admin.ModelAdmin):
     # pass
 
