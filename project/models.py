42d3db2d97b7c92fd1c1d34bad0afc977d162b78
Ravsimar Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2019-06-21 15:38:36 +0530

    adding projects in bulk logic

--- project/models.py
+++ project/models.py
@@ -4,12 +4,12 @@ from registration.models import Student
 
 # Create your models here.
 class Project(models.Model):
-    project_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=120)
-    url = models.URLField()
+    owner = models.CharField(max_length=120)
     level = models.PositiveSmallIntegerField(choices=((1,"Easy"),(2, "Medium"), (3, "Hard")), default = 2)
     mentors = models.ManyToManyField(Mentor, blank=True, null=True)
     students = models.ManyToManyField(Student, blank=True,null=True)
+
 
     def __str__(self):
         return self.name
--- project/views.py
+++ project/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 from .models import Project
-
+import requests
 # Create your views here.
+
+def project_info(name, owner):
+    url = "https://api.github.com/repos/" + owner + "/" + name
+    raw = requests.get(url = url)
+    json = raw.json()
+    return json
+
 def list_projects(request):
     projects = []
     if request.user.is_authenticated:
         for project in Project.objects.all():
-            tempd = {}
-            print(project.mentors.all())
-            tempd['project_id'] = project.project_id
+            tempd = project_info(project.name, project.owner)
             tempd['level'] = project.level
             tempd['name'] = project.name
-            tempd['url'] = project.url
             tempd['mentors'] = [mentor for mentor in project.mentors.all() ]
             tempd['students'] = [student for student in project.students.all() ]
             projects.append(tempd)
         return render(request, 'project/projectlist.html', {'projects': projects})
+
+def add_project(name, owner, level):
+    obj = Project(name = name, owner = owner, level = level)
+    obj.save()
+
+def add_projects_bulk(request, o_type, owner, level):
+    url = "https://api.github.com" + o_type +"s/" + owner + "/repos"
+    raw = requests.get(url = url)
+    json = raw.json()
+    for item in json:
+        owner, name = item['name'], item['owner']['login']
+        add_project(name, owner, level)
+    return
